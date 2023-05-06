d699815b02e501eda5988fbadc36168bf0067af2
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-05-07 00:57:33 +0200

    auth 1.1.2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from routes import users
+from routes import users, auth
 
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
--- routes/auth.py
+++ routes/auth.py
@@ -39,16 +39,45 @@ users_db = {
 
 def search_user(username: str):
     if username in users_db:
-        return UserDB(users_db[username])
-    return {"message": "Credenciales incorrectas"}
+        return UserDB(**users_db[username])
+    raise HTTPException(status_code=401,
+                        detail="Credenciales incorrectas - PASS FUNCION",
+                        headers={"WWW-authenticate": "Bearer"})
+
+
+def search_user_protected(username: str):
+    if username in users_db:
+        return User(**users_db[username])
+    raise HTTPException(status_code=401,
+                        detail="Credenciales incorrectas - PROTECTED FUNCION",
+                        headers={"WWW-authenticate": "Bearer"})
+
+
+async def current_user(token: str = Depends(auth_sys)):
+    user = search_user_protected(token)
+    if not user:
+        raise HTTPException(status_code=401,
+                            detail="Credenciales incorrectas - ASYNC FUNCION",
+                            headers={"WWW-authenticate": "Bearer"})
+    if user.disabled:
+        raise HTTPException(status_code=400,
+                            detail="usuario inactivo, contacte con soporte")
+    return user
 
 
 @router.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
     if not user_db:
-        raise HTTPException(status_code=400, detail="Credenciales incorrectas")
+        raise HTTPException(
+            status_code=400, detail="Credenciales incorrectas - USER")
     user = search_user(form.username)
     if not form.password == user.password:
-        raise HTTPException(status_code=400, detail="Credenciales icorrectas")
-    return {"access_token": , "token_type": "bearer"}
+        raise HTTPException(
+            status_code=400, detail="Credenciales icorrectas - PASS FORM")
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@router.get("/profile")
+async def profile(user: User = Depends(current_user)):
+    return user
